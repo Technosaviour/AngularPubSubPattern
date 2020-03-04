edc925b560fc916772447a8520cfef036a497521
Biswa kalyan Das <7573203+luckvicky@users.noreply.github.com>
2020-03-04 20:33:34 +0530

    Observable pattern
    Observable pattern  to support publish and subscribe in Angular

---
+++ ObserverService.ts
@@ -0,0 +1,41 @@
+import { Observer } from "rxjs";
+import { Subject } from 'rxjs/Subject';
+import { Type, Injectable } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+
+type Channels = Map<Type<any>, Subject<any>>;
+
+@Injectable()
+export class ObserverService
+{
+    private channels: Channels = new Map<Type<any>, Subject<any>>();
+
+    subscribe<T>(type: Type<T>): Observable<T> { 
+        if (!this.channels.has(type)) {
+            this.channels.set(type, new Subject<T>());
+        }
+        return this.getChannel(type).asObservable();
+    }
+
+    unsubscribe<T>(type: Type<T>): Observable<T> { 
+        if (this.channels.has(type)) {
+            this.channels.delete(type);
+        }
+        return new Observable();
+    }
+
+    publish<T>(type: Type<T>): void {
+        if (!this.channels.has(type)) {
+            this.channels.set(type, new Subject<T>());
+        }
+        this.getChannel(type).next();
+    }
+
+    private getChannel<T>(type: Type<T>) : Subject<any> {
+        const obj = this.channels.get(type);
+        if (obj) {
+            return obj
+        }
+        console.log(`No subscription found for channel with type ${type}`);
+    }
+}
